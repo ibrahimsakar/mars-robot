deeeb55908311b4a96ee9b18ac44c44d71b021da
İbrahim Şakar <ibrahim.sakar@setur.com.tr>
2020-11-22 23:10:50 +0300

    Add tests.

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,18 +1,29 @@
-using NUnit.Framework;
+using System;
+using Xunit;
+using MarsRobot;
 
 namespace Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
+        [Fact]
         public void Test1()
         {
-            Assert.Pass();
+            Robot robot = new Robot("1 2 N", "5 5");
+
+            robot.ExecuteCommand("LMLMLMLMM");
+
+            Assert.Equal("1 3 N", robot.x + " " + robot.y + " " + robot.direction);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Robot robot = new Robot("3 3 E", "5 5");
+
+            robot.ExecuteCommand("MMRMMRMRRM");
+
+            Assert.Equal("5 1 E", robot.x + " " + robot.y + " " + robot.direction);
         }
     }
 }
