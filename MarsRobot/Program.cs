729c478465c01e11690e2b58bde8012b42f4b3d9
İbrahim Şakar <ibrahim.sakar@setur.com.tr>
2020-11-22 23:15:47 +0300

    Update program.cs

--- MarsRobot/Program.cs
+++ MarsRobot/Program.cs
@@ -6,7 +6,17 @@ namespace MarsRobot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Robot robot = new Robot("1 2 N", "5 5");
+
+            robot.ExecuteCommand("LMLMLMLMM");
+            
+            Console.WriteLine("The robot on {0}, {1}, {2}", robot.x, robot.y, robot.direction);
+            
+            Robot robot2 = new Robot("3 3 E", "5 5");
+
+            robot2.ExecuteCommand("MMRMMRMRRM");
+            
+            Console.WriteLine("The robot on {0}, {1}, {2}", robot2.x, robot2.y, robot2.direction);
         }
     }
 }
